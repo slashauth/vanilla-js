095c1ecf41327fc0b39cdb8acd1a44f37581a7b7
Ned Rockson <nedrocks@gmail.com>
2022-07-05 18:20:11 -0700

    Updating package name

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,8 @@ const renderAppContainer = () => {
 };
 
 renderAppContainer();
+// NOTE: We are keeping this in here so it's easier to get
+// started.
 const wallet = new Wallet({
   appName: 'slashauth',
   infuraID: 'ed0a2b655d424e718cc0d2d1a65a056d',
